e1bcba461f8f3c0c9a93f63ffc8c6a70acd2e031
Rahul Raj <rahulraj@Rahuls-MacBook-Air.local>
2025-05-09 19:47:53 +0530

    find middle node question

---
+++ LinkedList/find_middle_node.py
@@ -0,0 +1,39 @@
+#find the middle node of the linked list without using length and with only one loop
+# Used two pointer slow(it moves one step in every iteration) and fast(it moves two steps in every iteration)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+    
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+
+print(my_linked_list.find_middle_node().value)
+            
