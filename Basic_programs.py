795caa281c2018c117228c74b382952c44494aea
Rahul Raj <rahulraj@Rahuls-MacBook-Air.local>
2025-06-09 13:23:38 +0530

    Same Basic Python Programs

--- Basic_programs.py
+++ Basic_programs.py
@@ -1,39 +1,39 @@
 # Function to reverse a string (Input: hello, Output: olleh)
-# def reverse_string(input):
-#     length = len(input)
-#     s = ""
-#     for i in range(length-1, -1, -1):
-#         s+=input[i]
-#     return s
+def reverse_string(input):
+    length = len(input)
+    s = ""
+    for i in range(length-1, -1, -1):
+        s+=input[i]
+    return s
 
-# print(reverse_string("hello"))
+print(reverse_string("hello"))
 
 # Function to check if a word is Palindrome (Input: MAM, Output: True)
-# def is_palindrome(input):
-#     length = len(input)
-#     s = ""
-#     for i in range(length-1, -1, -1):
-#         s+=input[i]
-#         #print(input[i])
-#     return s == input
+def is_palindrome(input):
+    length = len(input)
+    s = ""
+    for i in range(length-1, -1, -1):
+        s+=input[i]
+        #print(input[i])
+    return s == input
 
-# print(is_palindrome("MAM"))
+print(is_palindrome("MAM"))
 
 # Function to print words in reverse (Input: RAHUL RAJ, Output: RAJ RAHUL)
-# def reverse_word(input):
-#     rev = []
-#     word = ""
-#     for char in input:
-#         word+=char
-#         if char == " ":
-#             rev.append(word)
-#             word = ""
-#     rev.append(word)
-#     #print(rev, word)  
-#     for i in range(len(rev)-1,-1,-1):
-#         print(rev[i])
+def reverse_word(input):
+    rev = []
+    word = ""
+    for char in input:
+        word+=char
+        if char == " ":
+            rev.append(word)
+            word = ""
+    rev.append(word)
+    #print(rev, word)  
+    for i in range(len(rev)-1,-1,-1):
+        print(rev[i])
 
-# reverse_word("RAHUL RAJ")
+reverse_word("RAHUL RAJ")
     
 # Function to Return Top K repeated elements (with out using any built-in methods) (Input: [1,1,1,2,2,3,3,3,4,4,4,4], Output: RAJ RAHUL)
 def Top_k_elements(input):
